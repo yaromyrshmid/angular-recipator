24984910468ba46be448ca7d5c8339cbb9a717ad
yaromyrshmid <yaromyrshmid@gmail.com>
2020-01-28 11:31:32 +0200

    Routers reviewed

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Recipe } from "../recipe.model";
 import { ActivatedRoute, Params, Router } from "@angular/router";
 
